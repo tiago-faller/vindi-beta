dd7150bc33ab26229a750a37ad890153ad7e92d8
tiago-faller <79518335+tiago-faller@users.noreply.github.com>
2021-02-23 09:35:28 -0300

    Add files via upload

---
+++ vindi.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Plugin Name: Vindi WooCommerce 2 (BETA)
+ * Plugin URI: https://github.com/vindi/vindi-woocommerce
+ * Description: Adiciona o gateway de pagamento da Vindi para o WooCommerce.
+ * Author: Vindi
+ * Author URI: https://www.vindi.com.br
+ * Version: 1.1.1
+ * Requires at least: 4.4
+ * Tested up to: 5.6
+ * WC requires at least: 3.0.0
+ * WC tested up to: 4.9.0
+ * Text Domain: vindi-payment-gateway
+ *
+ * Domain Path: ./src/languages/
+ *
+ * @package Vindi
+ *
+ */
+
+if (!defined('ABSPATH')) {
+  exit; // Exit if accessed directly.
+}
+
+// Adding the development variables
+if(file_exists(plugin_dir_path(__FILE__) . '.env.php')) {
+  include plugin_dir_path(__FILE__) . '.env.php';
+}
+
+// Adding the variables
+require plugin_dir_path(__FILE__) . '/src/utils/DefinitionVariables.php';
+
+// Adding dependency validator
+require_once plugin_dir_path(__FILE__) . '/src/validators/Dependencies.php';
+
+require_once VINDI_PATH . 'src/VindiWoocommerce.php';
+
+if (!defined('VINDI_TESTS')) {
+  // In tests we run the instance manually.
+  // $GLOBALS['vindi'] = WC_Vindi_Payment::get_instance();
+}
